d4c21f425b85a109c3100c9369aeaef2072794f5
BilalMughal3993 <98820231+BilalMughal3993@users.noreply.github.com>
2022-02-20 21:18:21 +0500

    Checking its working

--- components/CounterApp.js
+++ components/CounterApp.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import { View,Text } from 'react-native'
 const CounterApp = () => {
   return (
-    <div>CounterApp</div>
+    <View>
+        <Text>Hello Word</Text>
+    </View>
   )
 }
 
